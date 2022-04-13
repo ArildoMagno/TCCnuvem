9e2d142c8df6b3321919a8c8edc0efbf346d702d
Arildo Magno <macedoarildo@hotmail.com>
2022-04-13 15:49:14 -0300

    Interpretation of Result

--- Projeto/CodePython/text-manipulation.py
+++ Projeto/CodePython/text-manipulation.py
@@ -1,17 +1,12 @@
 import spacy
 import wn
 from wn.similarity import wup
-from itertools import product
 
 # configure
 # wn.add('banco-own-pt/own-pt-lmf.xml')
 spacy.prefer_gpu()
 nlp = spacy.load("pt_core_news_lg")
 all_stop_words = nlp.Defaults.stop_words
-
-
-# 1 - implementar PD
-# 2 - passar palavra original junto com o lema para mostrar na hora do print
 
 
 def read_text(text):
@@ -28,7 +23,6 @@ def calculate_wu_palmer_similarity(word1, word2):
         synset2 = synset2[0]
         if synset1.pos == synset2.pos:
             value_similarity = wup(synset1, synset2, True)
-
     return value_similarity
 
 
@@ -37,45 +31,63 @@ def calculate_similarity_between_docs(doc_segmented1, doc_segmented2):
     for set1 in doc_segmented1:
         for set2 in doc_segmented2:
             similar_sets_temp = calculate_similarity_between_sets(set1, set2)
-            similar_sets.append((calculate_similarity_between_sets(set1, set2), set1, set2))
+            similar_sets.append((similar_sets_temp, set1, set2))
 
-            # no lugar desse IF vai jogar para o PD(A,B)
-            # e se PD der True, já joga p saida a similaridade entre os conjuntos
-            if similar_sets_temp[0] > 0.7:
-                print("Similaridade de:", similar_sets_temp[0], " entre ", set1, set2)
-            if similar_sets_temp[1] > 0.7:
-                print("Similaridade de:", similar_sets_temp[1], " entre ", set1, set2)
+            # secao 4.3.3 calculo
+            if plagiarism_detection_analyse(similar_sets_temp[0], similar_sets_temp[1]):
+                sw1 = show_words_from_set(set1)
+                sw2 = show_words_from_set(set2)
+                print("Similaridade:", round(similar_sets_temp[0], 2), " entre ", sw1, " do doc1 e", sw2, "do doc2")
     return similar_sets
 
 
-def plagiarism_detection_analyse():
-    print()
+def show_words_from_set(set_input):
+    sim = []
+    for i in set_input:
+        sim.append(i[0])
+    return sim
+
+
+def plagiarism_detection_analyse(average_ab, avegare_ba):
+    # calculo secao 4.3.3
+    p = 0.78
+    if average_ab >= p:
+        return True
+    else:
+        return False
 
 
 def calculate_similarity_between_sets(set1, set2):
-    similar_sets = []
     anB = []
     bmA = []
-    # algoritmo matematico descrito na secao 4.3.2
+    # calculo secao 4.3.2
 
     # anB: a1Bn, a2Bn, a3Bn
-    # calcula a relacao de cada elemento de a com todos os elementos do conjunto B
+    # relacao de cada elemento de A com todos os elementos do conjunto B
     for word1 in set1:
         temp_similarity = []
         for word2 in set2:
-            temp_similarity.append(calculate_wu_palmer_similarity(word1, word2))
+            # [1] = lemma
+            temp_similarity.append(calculate_wu_palmer_similarity(word1[1], word2[1]))
         anB.append(max(temp_similarity))
 
-    # calcula a relacao de cada elemento de b com todos os elementos do conjunto A
+    # relacao de cada elemento de B com todos os elementos do conjunto A
     for word2 in set2:
         temp_similarity = []
         for word1 in set1:
-            temp_similarity.append(calculate_wu_palmer_similarity(word2, word1))
+            temp_similarity.append(calculate_wu_palmer_similarity(word2[1], word1[1]))
         bmA.append(max(temp_similarity))
 
-    # calcula a media, para ter a relação entre AB e BA
+    # relação entre AB e BA
+    # FIX BUG ANOTADO NO PAPEL SOBRE O CALCULO ENVOLVENDO CONJUNTOS VAZIOS
+    # ELES ESTAO INTERFERINDO NA MEDIA, E DEVEM SER DESCONSIDERADOS NOS CALCULOS
+    # QUANDO OS 0(zeros) VEEM POR MOTIVOS DO CONJUNTO ESTAR VAZIO
+    # DO CONTRARIO É ZERO DA SIMILARIDADE MESMO E NAO DE ESTAR VAZIO
+    # TRATAR ISSO NO MÉTODO CALCULATE WU PALMER
     average_anB = sum(anB) / len(anB)
     average_bmA = sum(bmA) / len(bmA)
+    if average_bmA == 0.8 or average_anB == 0.8:
+        print()
     return average_anB, average_bmA
 
 
@@ -86,30 +98,8 @@ def tokenization_lematization_stopwordsremoval(text):
         if token.text not in all_stop_words and token.lemma_ not in all_stop_words \
                 and token.tag_ != "PUNCT" and token.tag_ != "SPACE" \
                 and token.lemma_ not in words_pos:
-            words_pos.append(token.lemma_)
+            words_pos.append((token.text, token.lemma_))
     return words_pos
-
-
-def execute():
-    doc_input1 = read_text("texto1.txt")
-    doc_input2 = read_text("texto2.txt")
-    n_gram = 3
-    # FEm
-    doc_tokenized_lematized_nostopwords1 = tokenization_lematization_stopwordsremoval(doc_input1)
-    doc_tokenized_lematized_nostopwords2 = tokenization_lematization_stopwordsremoval(doc_input2)
-    doc_segmented1 = ngrams(doc_tokenized_lematized_nostopwords1, n_gram)
-    doc_segmented2 = ngrams(doc_tokenized_lematized_nostopwords2, n_gram)
-
-    calculate_similarity_between_docs(doc_segmented1, doc_segmented2)
-    print("end")
-
-
-def search_synsets(word_source):
-    synset_word = wn.synsets(word_source)
-    print("From: ", word_source)
-    for word in synset_word:
-        word_syn = wn.synset(word.id)
-        print('syn-lemmas: ', word_syn.lemmas())
 
 
 def ngrams(input_ngrams, n):
@@ -117,6 +107,19 @@ def ngrams(input_ngrams, n):
     for i in range(len(input_ngrams) - n + 1):
         output.append(input_ngrams[i:i + n])
     return output
+
+
+def execute():
+    print("\nSimilariade (0~1) 0=Completamente diferentes, 1=Identicos ou Sinonimos\n\n")
+    doc_input1 = read_text("text-example1.txt")
+    doc_input2 = read_text("text-example2.txt")
+    n_gram = 5
+    # FEM
+    doc_tokenized_lematized_nostopwords1 = tokenization_lematization_stopwordsremoval(doc_input1)
+    doc_tokenized_lematized_nostopwords2 = tokenization_lematization_stopwordsremoval(doc_input2)
+    doc_segmented1 = ngrams(doc_tokenized_lematized_nostopwords1, n_gram)
+    doc_segmented2 = ngrams(doc_tokenized_lematized_nostopwords2, n_gram)
+    calculate_similarity_between_docs(doc_segmented1, doc_segmented2)
 
 
 execute()
