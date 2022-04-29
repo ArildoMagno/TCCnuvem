9228af43b1f650f9bc70c6d78845e63960f872be
Arildo Magno <macedoarildo@hotmail.com>
2022-04-29 19:57:23 -0300

    Update Pre Django Testes

--- ProjetoDjangoTeste/tutorialdjango/blog/admin.py
+++ ProjetoDjangoTeste/tutorialdjango/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "created", "updated")
