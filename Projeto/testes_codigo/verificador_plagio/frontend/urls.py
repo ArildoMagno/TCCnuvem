94e202e8d27ff13905d1e85e2730994b258a2607
Arildo Magno <macedoarildo@hotmail.com>
2022-07-18 19:39:24 -0300

    Remove PyCache

--- Projeto/testes_codigo/verificador_plagio/frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index
-
-urlpatterns = [
-    path('', index),
-    path('result', index),
-    path('homepage', index),
-    path('details', index),
-]
